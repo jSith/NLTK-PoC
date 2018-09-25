c9f045df5b2bd2d9362316bba306b1921803ff31
Jessica Smith <30677679+jSith@users.noreply.github.com>
2018-09-24 21:22:05 -0500

    Add clarifying comment

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 class Analysis:
-    def __init__(self, is_positive, is_neutral, is_negative):
+    def __init__(self, is_positive, is_neutral, is_negative): # This is a constructor
         self.is_positive = is_positive
         self.is_neutral = is_neutral
         self.is_negative = is_negative
