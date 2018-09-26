bc8b9764c918bcae7e844745a2f4038f43addbea
jSith <jwilmasmith@gmail.com>
2018-09-26 12:39:38 -0500

    Add information on how to run

--- analysis.py
+++ analysis.py
@@ -1,5 +1,8 @@
 class Analysis:
-    def __init__(self, is_positive, is_neutral, is_negative): # This is a constructor
+    def __init__(self, is_positive, is_neutral, is_negative):  # This is a constructor
         self.is_positive = is_positive
         self.is_neutral = is_neutral
         self.is_negative = is_negative
+
+    # It is possible to use getters and setters in Python
+        # However, it is considered best practice to allow attributes to be accessed directly
